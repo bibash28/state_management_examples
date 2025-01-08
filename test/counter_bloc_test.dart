b4d93f4132b626e616d5094da203cb81d0435557
Bibash Shrestha <bibashshrestha@gmail.com>
2025-01-08 09:34:12 +0545

    Add tests for bloc counter

--- test/counter_bloc_test.dart
+++ test/counter_bloc_test.dart
@@ -4,6 +4,11 @@ import 'package:state_management_example/bloc/bloc/counter/counter_bloc.dart';
 
 void main() {
   group('CounterBloc', () {
+    test('initial state is CounterCubitInitial', () {
+      final cubit = CounterBloc();
+      expect(cubit.state, CounterInitial(action: 'Initialized', count: 0));
+    });
+
     blocTest(
       'emits [] when nothing is called',
       build: () => CounterBloc(),
@@ -15,6 +20,13 @@ void main() {
       build: () => CounterBloc(),
       act: (bloc) => bloc.add(IncrementEvent()),
       expect: () => [CounterIncreased(action: 'Increased', count: 1)],
+    );
+
+    blocTest(
+      'emits [1] when decrement is called',
+      build: () => CounterBloc(),
+      act: (bloc) => bloc.add(DecrementEvent()),
+      expect: () => [CounterDecreased(action: 'Decreased', count: -1)],
     );
 
     blocTest(
